36a143d958457b176f6c9ddfd9f266754faadf1d
shantanu-exe <shnigam@ucsd.edu>
2025-05-24 18:49:45 -0700

    my base working setup

--- start_linux.sh
+++ start_linux.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-# Copyright (c) 2024 ETH Zurich and University of Bologna.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Authors:
-# - Philippe Sauter <phsauter@iis.ee.ethz.ch>
 
-env UID=$(id -u) GID=$(id -g) docker compose pull pulp-docker
+# Use different variable names to avoid clashing with readonly shell variables
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
 
-env UID=$(id -u) GID=$(id -g) docker compose run --rm \
+docker compose build \
+  --build-arg UID=$USER_ID \
+  --build-arg GID=$GROUP_ID \
+  pulp-docker
+
+docker compose run --rm \
+  -it \
   -e PS1="\[\033[01;32m\]osic:\[\033[00m\]\[\033[01;34m\]\w\[\033[00m\] $" \
-  -e DISPLAY=$DISPLAY \
+  -e DISPLAY=docker.for.mac.host.internal:0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   pulp-docker
